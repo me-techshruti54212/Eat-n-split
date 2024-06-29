7ac8b5020e754bdaa07dcc27c400dec2db7c001f
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-06-29 09:02:38 +0530

    friendslist

--- src/FriendsList.js
+++ src/FriendsList.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import Friend from './Friend'
-const FriendsList = ({friends,onSelection,selectedFriend}) => {
+import React from "react";
+import Friend from "./Friend";
+const FriendsList = ({ friends, onSelection, selectedFriend }) => {
   return (
     <ul>
-        {friends.map((friend)=><Friend friend={friend} key={friend.id} onSelection={onSelection} selectedFriend={selectedFriend}/>)}
+      {friends.map((friend) => (
+        <Friend
+          friend={friend}
+          key={friend.id}
+          onSelection={onSelection}
+          selectedFriend={selectedFriend}
+        />
+      ))}
     </ul>
-  )
-}
+  );
+};
 
-export default FriendsList
+export default FriendsList;
